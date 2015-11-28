e75dba6054e423437691d6554d9cc4c0cfef079a
shimao <durashorc@yahoo.com>
2015-11-28 06:05:23 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conzalt::Application.config.secret_key_base = '2ef99b423be4b7578f2379dea880f9b90ad47e3490ae3383d751b63edfa023a915610e7c46db6188e233ed3aec3dbf7cb8fca2556e0ba43b087e43da86370282'
